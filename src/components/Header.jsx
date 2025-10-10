9554f874e7135fe274e4d89ad8d6ccafcbae14f2
JOSH-MOTION <joshuadoe168@gmail.com>
2025-10-10 17:47:10 +0000

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -69,7 +69,7 @@ const Header = () => {
             onClick={closeMenu} 
             className="block hover:text-gray-200 py-2 px-2 rounded transition-all duration-200 hover:bg-red-700"
           >
-            Hom
+            Home
           </Link>
           <Link 
             to="/documentaries" 
